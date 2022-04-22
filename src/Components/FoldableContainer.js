1bf0b5b1f8aa73b6ed7999a929eb94f93545902a
Karolin Thunman <karolinthunman@Karolins-MacBook-Pro-2.local>
2022-04-22 20:29:31 +0200

    start

--- src/Components/FoldableContainer.js
+++ src/Components/FoldableContainer.js
@@ -1,21 +1,32 @@
 import { useState } from "react";
+import arrowDownIcon from "../icons/caret_down_icon.svg";
+import arrowRightIcon from "../icons/caret_right_icon.svg";
 
 const FoldableContainer = ({ children, label }) => {
   const [expanded, setExpanded] = useState(true);
   return (
     <div className="expandable-container">
       <div
-        className="expandable-container-label"
         onClick={() => setExpanded(!expanded)}
         onKeyDown={(e) => (e.code === "Enter" ? setExpanded(!expanded) : null)}
         role="button"
         tabIndex={0}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          padding: "10px",
+          border: "1px solid #888",
+        }}
       >
+        <img
+          src={expanded ? arrowDownIcon : arrowRightIcon}
+          height="30px"
+          alt="mySvgImage"
+          style={{ marginRight: "10px" }}
+        />
         {label}
       </div>
-      {expanded ? (
-        <div className="expandable-container-body">{children}</div>
-      ) : null}
+      {expanded ? <div style={{ padding: "10px" }}>{children}</div> : null}
     </div>
   );
 };
