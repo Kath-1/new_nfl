63dc46a44d5d0080cad8a4c0821cbbacce6fa0db
Karolin Thunman <karolinthunman@Karolins-MacBook-Pro-2.local>
2022-07-10 12:28:34 +0200

    skeletons basic pages

--- client/src/BetGame.js
+++ client/src/BetGame.js
@@ -5,8 +5,9 @@ const BetGame = ({ game }) => {
     <fieldset
       style={{
         display: "flex",
-
         padding: "20px 0",
+        justifyContent: "center",
+        border: "none",
       }}
     >
       <label className="form-control">
@@ -15,18 +16,40 @@ const BetGame = ({ game }) => {
           id={game.id + game.awayTeam.nickname}
           name={game.id}
         />
-        <span>{game.awayTeam.nickname}</span>
+        <div>
+          <img
+            style={{ height: "40px" }}
+            alt="team logo"
+            src={game.awayTeam.logo}
+          />
+          {game.awayTeam.shortName}
+        </div>
       </label>
+      <div style={{ display: "flex", alignItems: "center", fontSize: "2rem" }}>
+        @
+      </div>
       <label className="form-control">
+        <span style={{ position: "absolute", right: "-20px" }}>$</span>
         <input
           type="radio"
           id={game.id + game.homeTeam.nickname}
           name={game.id}
         />
-        <span>{game.homeTeam.nickname}</span>
+        <div>
+          {game.homeTeam.shortName}
+          <img
+            style={{ height: "40px" }}
+            alt="team logo"
+            src={game.awayTeam.logo}
+          />
+        </div>
       </label>
-
-      <input type="number" min="20" style={{ width: "60px" }} />
+      <input
+        type="number"
+        min="20"
+        max="9999"
+        style={{ width: "100px", fontSize: "1.5rem", paddingLeft: "25px" }}
+      />
     </fieldset>
   );
 };
